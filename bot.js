f03d110aa77368000104d749789bec6890eec370
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 00:40:55 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.client();
+
+client.on('ready', () => {
+   console.log('I am ready!');
+});
+
+client.on('message', message => {
+   if (message.content === 'ping') {
+      message.reply('pong');
+   }
+});
+
+client.login(process.env.BOT_TOKEN);
