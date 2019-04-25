115fd4495e661421140ca2155a6f32daa054a7f0
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 03:01:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Online');
 });
 
 client.on('message', msg => {
@@ -11,5 +11,22 @@ client.on('message', msg => {
   	}
 });
 
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `#`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split('!');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            case 'Zentrales Thanalan':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Central Thanalan'
+                });
+        }
+   }
+});
 
 client.login(process.env.BOT_TOKEN);
