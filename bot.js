ad4b7896ecb8b4081af511dc92cb88b5440eaf76
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 05:23:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,8 +14,7 @@ bot.on('ready', () => {
 
 bot.on("message", area => {
 	if (area.content.startsWith("Zentrales Thanalan")) {
-		//const coord = area.content.slice(18,area.length);
-		const coord = area.content.slice(2,area.length);
+		const coord = area.content.slice(18,area.length);
 		area.channel.send("Central Thanalan " + coord);
 		console.log("String: " + coord);
 	}		
