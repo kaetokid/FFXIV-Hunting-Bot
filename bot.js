e0c18c5b0e36dada7728e5dea712eca5e7d537f8
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 02:29:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const BOT_TOKEN = proces.env.BOT_TOKEN
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+    	msg.reply('pong');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(BOT_TOKEN).catch(err => console.log(err));
