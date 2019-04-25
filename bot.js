f9a6cc008ce0b7fb1391e13b1a86cbc7ba8231eb
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 05:47:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,9 +16,10 @@ bot.on("message", area => {
 	if (area.content.startsWith("Zentrales Thanalan")) {
 		const coord = area.content.slice(18,area.length);
 		let user = area.author;
+		//console.log("user: " + user);
 		area.delete();
-		area.channel.send(user.toString() + ": Central Thanalan " + coord);
-		//console.log("String: " + coord);
+		area.channel.send(user + ": Central Thanalan " + coord);
+		//console.log("coord: " + coord);
 	}		
 });
 
