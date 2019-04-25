6c8b7a71ea6b9c1ae97b63d210347532aaedf678
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 03:17:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
         args = args.splice(1);
         switch(cmd) {
             case 'Zentrales Thanalan':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Central Thanalan'
                 });
