8103a8e97a7a768f510d0a7a80291cf0908336b3
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 03:03:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', msg => {
   	}
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `#`
     if (message.substring(0, 1) == '!') {
