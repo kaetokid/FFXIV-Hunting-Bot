f188e78cf46e516e83a619559c6478aad461cf87
kaetokid <49963499+kaetokid@users.noreply.github.com>
2019-04-25 02:57:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const BOT_TOKEN = proces.env.BOT_TOKEN
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -13,4 +12,4 @@ client.on('message', msg => {
 });
 
 
-client.login(BOT_TOKEN).catch(err => console.log(err));
+client.login(process.env.BOT_TOKEN);
